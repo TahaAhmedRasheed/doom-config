9d0c813d8699eb784d7070a89bd7bb565c4ce6f7
Taha Ahmed Rasheed <tahaahmedrasheedpk@gmail.com>
2021-09-15 08:17:31 +0500

    Open ALL frames maximized, not just the first one.

--- config.el
+++ config.el
@@ -63,7 +63,7 @@
 ;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
 ;; they are implemented.
 
-(add-to-list 'initial-frame-alist '(fullscreen . maximized))
+(add-to-list 'default-frame-alist '(fullscreen . maximized))
 
 (setq confirm-kill-emacs nil)
 
