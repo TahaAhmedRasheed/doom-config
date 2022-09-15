1dd5e53bf4515d76f68a4873f3ed23206b87e3e6
Taha Ahmed Rasheed <tahaahmedrasheedpk@gmail.com>
2022-09-15 11:55:45 +0500

    Vanilla config for WSL

--- config.el
+++ config.el
@@ -3,52 +3,60 @@
 ;; Place your private configuration here! Remember, you do not need to run 'doom
 ;; sync' after modifying this file!
 
-(if (eq system-type 'gnu/linux)
-    (setq default-directory "/mnt/c/Users/HAIER"))
 
 ;; Some functionality uses this to identify you, e.g. GPG configuration, email
-;; clients, file templates and snippets.
+;; clients, file templates and snippets. It is optional.
 (setq user-full-name "John Doe"
       user-mail-address "john@doe.com")
 
-;; Doom exposes five (optional) variables for controlling fonts in Doom. Here
-;; are the three important ones:
+;; Doom exposes five (optional) variables for controlling fonts in Doom:
 ;;
-;; + `doom-font'
-;; + `doom-variable-pitch-font'
-;; + `doom-big-font' -- used for `doom-big-font-mode'; use this for
+;; - `doom-font' -- the primary font to use
+;; - `doom-variable-pitch-font' -- a non-monospace font (where applicable)
+;; - `doom-big-font' -- used for `doom-big-font-mode'; use this for
 ;;   presentations or streaming.
+;; - `doom-unicode-font' -- for unicode glyphs
+;; - `doom-serif-font' -- for the `fixed-pitch-serif' face
 ;;
-;; They all accept either a font-spec, font string ("Input Mono-12"), or xlfd
-;; font string. You generally only need these two:
-
-;; An integral `:size' is interpreted as pixels. Floating-point numbers are
-;; interpreted as points.
-
-(setq fira-spec (font-spec :name "Fira Code" :size 13 :weight 'semi-light))
-
-(setq jetbrains-spec (font-spec :name "JetBrains Mono" :size 13 :weight 'light))
-
-(setq source-spec (font-spec :name "Source Code Pro" :size 10.2 :weight 'regular))
-
-(setq doom-font source-spec
-      doom-variable-pitch-font (font-spec :family "sans" :size 13))
+;; See 'C-h v doom-font' for documentation and more examples of what they
+;; accept. For example:
+;;
+;;(setq doom-font (font-spec :family "Fira Code" :size 12 :weight 'semi-light)
+;;      doom-variable-pitch-font (font-spec :family "Fira Sans" :size 13))
+;;
+;; If you or Emacs can't find your font, use 'M-x describe-font' to look them
+;; up, `M-x eval-region' to execute elisp code, and 'M-x doom/reload-font' to
+;; refresh your font settings. If Emacs still can't find your font, it likely
+;; wasn't installed correctly. Font issues are rarely Doom issues!
 
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
-(setq doom-theme 'doom-gruvbox)
-
-;; If you use `org' and don't want your org files in the default location below,
-;; change `org-directory'. It must be set before org loads!
-(setq org-directory "~/org/")
+(setq doom-theme 'doom-one)
 
 ;; This determines the style of line numbers in effect. If set to `nil', line
 ;; numbers are disabled. For relative line numbers, set this to `relative'.
 (setq display-line-numbers-type t)
 
+;; If you use `org' and don't want your org files in the default location below,
+;; change `org-directory'. It must be set before org loads!
+(setq org-directory "~/org/")
 
-;; Here are some additional functions/macros that could help you configure Doom:
+
+;; Whenever you reconfigure a package, make sure to wrap your config in an
+;; `after!' block, otherwise Doom's defaults may override your settings. E.g.
+;;
+;;   (after! PACKAGE
+;;     (setq x y))
+;;
+;; The exceptions to this rule:
+;;
+;;   - Setting file/directory variables (like `org-directory')
+;;   - Setting variables which explicitly tell you to set them before their
+;;     package is loaded (see 'C-h v VARIABLE' to look up their documentation).
+;;   - Setting doom variables (which start with 'doom-' or '+').
+;;
+;; Here are some additional functions/macros that will help you configure Doom.
 ;;
 ;; - `load!' for loading external *.el files relative to this one
 ;; - `use-package!' for configuring packages
@@ -61,146 +69,8 @@
 ;; To get information about any of these functions/macros, move the cursor over
 ;; the highlighted symbol at press 'K' (non-evil users must press 'C-c c k').
 ;; This will open documentation for it, including demos of how they are used.
+;; Alternatively, use `C-h o' to look up a symbol (functions, variables, faces,
+;; etc).
 ;;
 ;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
 ;; they are implemented.
-
-(add-to-list 'default-frame-alist '(fullscreen . fullscreen))
-
-(setq confirm-kill-emacs nil)
-
-(after! lsp-clangd
-  (add-to-list 'lsp-clients-clangd-args '"--clang-tidy"))
-
-(after! cc-mode
-  (setq c-basic-offset 2))
-
-(setq evil-want-fine-undo t)
-
-(setq evil-snipe-scope 'buffer)
-
-(setq evil-cross-lines t)
-(setq explicit-shell-file-name "C:\\Program Files\\PowerShell\\7\\pwsh.exe")
-(setq explicit-powershell.exe-args '("-NoLogo"))
-(setq comint-process-echoes t)
-
-(map!
- :after shell
- :map shell-mode-map
- :localleader
- :desc "Clear shell" "c" #'comint-clear-buffer
- :desc "Stop command" "s" #'comint-interrupt-subjob)
-
-(display-time-mode 1)
-
-(after! org
-  (plist-put org-format-latex-options :scale 1.3))
-
-(after! web-mode
-  (setq web-mode-markup-indent-offset 2))
-
-(after! python-mode
-  (setq flycheck-python-pycompile-executable "python"))
-
-;; This overrides org-mode's `+org/shift-return'
-(defun open-below-without-insert (count)
-  "Insert a new line below point without switching to Insert state.
-The insertion will be repeated COUNT times."
-  (interactive "p")
-  (evil-open-below count)
-  (evil-normal-state)
-  (message ""))
-
-(map! :map evil-normal-state-map "<S-return>" #'open-below-without-insert)
-
-(add-hook 'dired-mode-hook 'dired-hide-details-mode)
-
-;; Remove the icons on the dashboard
-;;(dolist (section +doom-dashboard-menu-sections)
-;;  (let ((section-plist (cdr section)))
-;;    (plist-put section-plist :icon nil)))
-
-(remove-hook '+doom-dashboard-functions #'doom-dashboard-widget-footer)
-
-(beacon-mode 1)
-(setq beacon-blink-delay 0.3
-      beacon-blink-duration 0.2)
-
-;; (setq langtool-language-tool-server-jar
-;;       "C:\ProgramData\chocolatey\lib\languagetool\tools\LanguageTool-5.1\languagetool-server.jar")
-
-(after! ox-latex
-  (add-to-list
-   'org-latex-classes
-   `("deeparticle"
-     ,(concat
-      "\\documentclass[11pt]{article}\n"
-      "\\usepackage{enumitem}\n"
-      "\\newenvironment{deepsection}[1]{\\begin{enumerate}[label={}] \\item \\textbf{#1} \\newline}{\\end{enumerate}}")
-     ("\\section{%s}" . "\\section*{%s}")
-     ("\\subsection{%s}" . "\\subsection*{%s}")
-     ("\\subsubsection{%s}" . "\\subsubsection*{%s}")
-     ("\\paragraph{%s}" . "\\paragraph*{%s}")
-     ("\\subparagraph{%s}" . "\\subparagraph*{%s}")
-     ("\\begin{deepsection}{%s}" "\\end{deepsection}" "\\begin{deepsection}{%s}" "\\end{deepsection}")
-     ("\\begin{deepsection}{%s}" "\\end{deepsection}" "\\begin{deepsection}{%s}" "\\end{deepsection}")
-     ("\\begin{deepsection}{%s}" "\\end{deepsection}" "\\begin{deepsection}{%s}" "\\end{deepsection}")
-     ("\\begin{deepsection}{%s}" "\\end{deepsection}" "\\begin{deepsection}{%s}" "\\end{deepsection}")
-     ("\\begin{deepsection}{%s}" "\\end{deepsection}" "\\begin{deepsection}{%s}" "\\end{deepsection}")
-     ("\\begin{deepsection}{%s}" "\\end{deepsection}" "\\begin{deepsection}{%s}" "\\end{deepsection}")
-     ("\\begin{deepsection}{%s}" "\\end{deepsection}" "\\begin{deepsection}{%s}" "\\end{deepsection}")
-     ("\\begin{deepsection}{%s}" "\\end{deepsection}" "\\begin{deepsection}{%s}" "\\end{deepsection}")
-     ("\\begin{deepsection}{%s}" "\\end{deepsection}" "\\begin{deepsection}{%s}" "\\end{deepsection}")
-     ("\\begin{deepsection}{%s}" "\\end{deepsection}" "\\begin{deepsection}{%s}" "\\end{deepsection}"))))
-
-(add-hook 'org-mode-hook #'xenops-mode)
-(setq xenops-reveal-on-entry t)
-
-(defun open-wt-here ()
-  "Open Windows Terminal in current directory."
-  (interactive)
-  (call-process "cmd" nil 0 nil "/c" "wt" "-d" (expand-file-name default-directory)))
-
-;; Passes a command of the form `cmd "/c wt -d C:\path\to\dir"' to `powershell start'
-;; -Verb runAs lets wt run with administrator privileges
-;; It's unclear why powershell has to ask cmd to open wt; asking powershell to open
-;; wt directly doesn't seem to work for some reason
-(defun open-admin-wt-here ()
-  "Open Windows Terminal with administrator privileges in current directory."
-  (interactive)
-  (call-process
-   "powershell"
-   nil
-   0
-   nil
-   "start"
-   "cmd"
-   (concat "\"/c wt -d " (expand-file-name default-directory) "\"")
-   "-Verb"
-   "runAs"))
-
-(defun open-explorer-here ()
-  "Open Windows explorer in current directory."
-  (interactive)
-  (shell-command "start .")
-  (message ""))
-
-;; FIXME: Descriptions for the ot and oT bindings are overwritten
-(map!
- :leader
- :desc "Open Windows Terminal here" "ot" #'open-wt-here
- :desc "Open Windows Terminal here as admin" "oT" #'open-admin-wt-here
- :desc "Toggle shell popup" "os" #'+shell/toggle
- :desc "Open shell here" "oS" #'+shell/here
- "oe" #'open-explorer-here)
-
-;; Prevents evil delete commands from copying text in addition to removing it.
-;; It does this by making each delete command copy text to the "black hole"
-;; register (_), which leaves the system clipboard and other registers untouched.
-;; Use the " register explicitly (e.g. ""diw) if the old behaviour is desired.
-(defadvice! my-evil-delete-default-to-black-hole-a
-  (fn beg end &optional type register yank-handler)
-  "Advise `evil-delete' to set default REGISTER to the black hole register."
-  :around #'evil-delete
-  (unless register (setq register ?_))
-  (funcall fn beg end type register yank-handler))
--- init.el
+++ init.el
@@ -32,8 +32,8 @@
        doom              ; what makes DOOM look the way it does
        doom-dashboard    ; a nifty splash screen for Emacs
        ;;doom-quit         ; DOOM quit-message prompts when you quit Emacs
-       ;;(emoji +unicode)  ; ??
-       ;; hl-todo           ; highlight TODO/FIXME/NOTE/DEPRECATED/HACK/REVIEW
+       ;;(emoji +unicode)  ; 🙂
+       hl-todo           ; highlight TODO/FIXME/NOTE/DEPRECATED/HACK/REVIEW
        ;;hydra
        ;;indent-guides     ; highlighted indent columns
        ;;ligatures         ; ligatures and symbols to make your code pretty again
@@ -46,7 +46,7 @@
        ;;tabs              ; a tab bar for Emacs
        ;;treemacs          ; a project drawer, like neotree but cooler
        ;;unicode           ; extended unicode support for various languages
-       vc-gutter         ; vcs diff in the fringe
+       (vc-gutter +pretty) ; vcs diff in the fringe
        vi-tilde-fringe   ; fringe tildes to mark beyond EOB
        ;;window-select     ; visually switch windows
        workspaces        ; tab emulation, persistence & separate workspaces
@@ -56,7 +56,7 @@
        (evil +everywhere); come to the dark side, we have cookies
        file-templates    ; auto-snippets for empty files
        fold              ; (nigh) universal code folding
-       ;; (format +onsave)  ; automated prettiness
+       ;;(format +onsave)  ; automated prettiness
        ;;god               ; run Emacs commands without modifier keys
        ;;lispy             ; vim for lisp, for people who don't like vim
        ;;multiple-cursors  ; editing in many places at once
@@ -75,13 +75,13 @@
 
        :term
        ;;eshell            ; the elisp shell that works everywhere
-       shell             ; simple shell REPL for Emacs
+       ;;shell             ; simple shell REPL for Emacs
        ;;term              ; basic terminal emulator for Emacs
        ;;vterm             ; the best terminal emulation in Emacs
 
        :checkers
        syntax              ; tasing you for every semicolon you forget
-       (spell +flyspell +hunspell +everywhere) ; tasing you for misspelling mispelling
+       ;;(spell +flyspell) ; tasing you for misspelling mispelling
        ;;grammar           ; tasing grammar mistake every you make
 
        :tools
@@ -94,8 +94,8 @@
        ;;ein               ; tame Jupyter notebooks with emacs
        (eval +overlay)     ; run code, run (also, repls)
        ;;gist              ; interacting with github gists
-       (lookup +dictionary)              ; navigate your code and its documentation
-       lsp               ; M-x vscode
+       lookup              ; navigate your code and its documentation
+       ;;lsp               ; M-x vscode
        magit             ; a git porcelain for Emacs
        ;;make              ; run make tasks from Emacs
        ;;pass              ; password manager for nerds
@@ -105,7 +105,7 @@
        ;;taskrunner        ; taskrunner for all your projects
        ;;terraform         ; infrastructure as code
        ;;tmux              ; an API for interacting with tmux
-       tree-sitter       ; syntax and parsing, sitting in a tree...
+       ;;tree-sitter       ; syntax and parsing, sitting in a tree...
        ;;upload            ; map local to remote projects via ssh/ftp
 
        :os
@@ -115,8 +115,8 @@
        :lang
        ;;agda              ; types of types of types of types...
        ;;beancount         ; mind the GAAP
-       (cc +lsp)         ; C > C++ == 1
-       clojure           ; java with a lisp
+       ;;(cc +lsp)         ; C > C++ == 1
+       ;;clojure           ; java with a lisp
        ;;common-lisp       ; if you've seen one lisp, you've seen them all
        ;;coq               ; proofs-as-programs
        ;;crystal           ; ruby at the speed of c
@@ -140,12 +140,12 @@
        ;;(haskell +lsp)    ; a language that's lazier than I am
        ;;hy                ; readability of scheme w/ speed of python
        ;;idris             ; a language you can depend on
-       json              ; At least it ain't XML
-       (java +lsp)       ; the poster child for carpal tunnel syndrome
-       (javascript +lsp)        ; all(hope(abandon(ye(who(enter(here))))))
+       ;;json              ; At least it ain't XML
+       ;;(java +lsp)       ; the poster child for carpal tunnel syndrome
+       ;;javascript        ; all(hope(abandon(ye(who(enter(here))))))
        ;;julia             ; a better, faster MATLAB
        ;;kotlin            ; a better, slicker Java(Script)
-       (latex +lsp)             ; writing papers in Emacs has never been so fun
+       ;;latex             ; writing papers in Emacs has never been so fun
        ;;lean              ; for folks with too much to prove
        ;;ledger            ; be audit you can be
        ;;lua               ; one-based indices? one-based indices
@@ -157,14 +157,14 @@
        ;;php               ; perl's insecure younger brother
        ;;plantuml          ; diagrams for confusing people more
        ;;purescript        ; javascript, but functional
-       (python +lsp +pyright +pyenv)            ; beautiful is better than ugly
+       ;;python            ; beautiful is better than ugly
        ;;qt                ; the 'cutest' gui framework ever
        ;;racket            ; a DSL for DSLs
        ;;raku              ; the artist formerly known as perl6
        ;;rest              ; Emacs as a REST client
        ;;rst               ; ReST in peace
        ;;(ruby +rails)     ; 1.step {|i| p "Ruby is #{i.even? ? 'love' : 'life'}"}
-       ;;rust              ; Fe2O3.unwrap().unwrap().unwrap().unwrap()
+       ;;(rust +lsp)       ; Fe2O3.unwrap().unwrap().unwrap().unwrap()
        ;;scala             ; java, but good
        ;;(scheme +guile)   ; a fully conniving family of lisps
        sh                ; she sells {ba,z,fi}sh shells on the C xor
@@ -172,8 +172,8 @@
        ;;solidity          ; do you need a blockchain? No.
        ;;swift             ; who asked for emoji variables?
        ;;terra             ; Earth and Moon in alignment for performance.
-       web               ; the tubes
-       yaml              ; JSON, but readable
+       ;;web               ; the tubes
+       ;;yaml              ; JSON, but readable
        ;;zig               ; C, but simpler
 
        :email
